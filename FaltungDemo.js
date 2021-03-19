94cc8bc682fcf430f12b0f320fc2b83b77ce641c
MoRaMo11 <80282160+MoRaMo11@users.noreply.github.com>
2021-03-19 01:51:41 +0100

    Update FaltungDemo.js

--- FaltungDemo.js
+++ FaltungDemo.js
@@ -1,11 +1,6 @@
-//__________________________________________Funktionen-FuncF__________________________________________
+/*_______________________________________Funktions-FuncF difinition with ax and ay as variables to change the value of the functions________________________*/
 var funcF;
-var funcG;
-Sprung = function funcG(t) {
-    var ax = A.X(),
-        ay = A.Y();
-    return ay * (1 / (1 + Math.exp(-999 * (ax * t))));
-};
+
 Dreieck = function funcF(t) {
     var ax = A.X(),
         ay = A.Y();
@@ -19,182 +14,142 @@ Rechteck = function funcF(t) {
         ay = A.Y();
     return ay * (1 / (1 + Math.exp(-999 * (ax * t + 1)))) - ay * (1 / (1 + Math.exp(-999 * (ax * t - 1))));
 };
-Rampe = function funcG(t) {
+Gauss = function funcF(t) {
     var ax = A.X(),
         ay = A.Y();
-    return ay * t * (1 / (1 + Math.exp(-999 * (t))));
+    return 2.5 * ay * (1 / Math.sqrt(2 * Math.PI)) * Math.exp(-0.5 * t * t * ax);
 };
-Sin = function funcG(t) {
+Sinc = function funcF(t) {
     var ax = A.X(),
         ay = A.Y();
-    return ay * Math.sin(ax * (t + 1.5));
+    return ay * Math.sin(Math.PI * t * 2 * ax) / (Math.PI * t * 2 * ax);
 };
-/*Gauß:Dichtefunktion der Standardnormalverteilung mit der Variabeln ay und ax*/
-Gauss = function funcG(t) {
+Saegezahn = function funcF(t) {
     var ax = A.X(),
         ay = A.Y();
-    return ay * (1 / Math.sqrt(2 * Math.PI)) * Math.exp(-0.5 * t * t * ax);
+    return ay * (((1 / (1 + Math.exp(-999 * (ax * t + 1)))) - (1 / (1 + Math.exp(-999 * (ax * t - 1))))) / 2) * (1 - ax * t);
 };
-Sinc = function funcG(t) {
-    var ax = A.X(),
-        ay = A.Y();
-    return ay * Math.sin(Math.PI * t * ax) / (Math.PI * t * ax);
+funcF = Rechteck; // default value for funcF
+
+/*____________________________________Funktions-FuncF difinition with ax and ay as variables to change the value of the functions__________________________*/
+var funcG;
+
+Dreieck2 = function funcF(t) {
+    var ax = A2.X(),
+        ay = A2.Y();
+    return ay * ((ax * t + 1) * (1 / (1 + Math.exp(-Infinity * (ax * t + 1)))) -
+        2 * ((ax * t) * (1 / (1 + Math.exp(-Infinity * (ax * t))))) -
+        ((ax * t - 1) * (1 / (1 + Math.exp(-Infinity * (ax * t - 1))))) +
+        2 * (ax * t - 1) * (1 / (1 + Math.exp(-Infinity * (ax * t - 1)))));
 };
-Saegezahn = function funcG(t) {
-    var ax = A.X(),
-        ay = A.Y();
-    return ay * ((1 / (1 + Math.exp(-999 * (t + 1)))) - (1 / (1 + Math.exp(-999 * (t - 1)))) / 2) * (1 - t);
+Rechteck2 = function funcF(t) {
+    var ax = A2.X(),
+        ay = A2.Y();
+    return ay * (1 / (1 + Math.exp(-999 * (ax * t + 1)))) - ay * (1 / (1 + Math.exp(-999 * (ax * t - 1))));
+};
+Gauss2 = function funcF(t) {
+    var ax = A2.X(),
+        ay = A2.Y();
+    return 2.5 * ay * (1 / Math.sqrt(2 * Math.PI)) * Math.exp(-0.5 * t * t * ax);
+};
+Sinc2 = function funcF(t) {
+    var ax = A2.X(),
+        ay = A2.Y();
+    return ay * Math.sin(Math.PI * t * 2 * ax) / (Math.PI * t * 2 * ax);
+};
+Saegezahn2 = function funcF(t) {
+    var ax = A2.X(),
+        ay = A2.Y();
+    return ay * (((1 / (1 + Math.exp(-999 * (ax * t + 1)))) - (1 / (1 + Math.exp(-999 * (ax * t - 1))))) / 2) * (1 - ax * t);
 };
 
-funcF = Dreieck;
-funcG = Rechteck;
-/*___________________________________________ChangeFunc___________________________________________*/
+funcG = Rechteck2; // default value for funcG
+/*____________________________________________________________________________ChangeFunc_______________________________________________________________________*/
 changeFunction = function(value, id) {
-        switch (value) {
-            case "Sprung":
-                func = Sprung;
-                break;
-            case "Rampe":
-                func = Rampe;
-                break;
-            case "Rechteck":
-                func = Rechteck;
-                break;
-            case "Dreieck":
-                func = Dreieck;
-                break;
-            case "Sägezahn":
-                func = Saegezahn;
-                break;
-            case "Sin":
-                func = Sin;
-                break;
-            case "Sinc":
-                func = Sinc;
-                break;
-            case "Gauß":
-                func = Gauss;
-                break;
-        }
         if (id == "functionList1") {
-            funcF = func;
+            funcF = eval(value);
             board1.removeObject(board1.elementsByName.funcF);
-            board1.create('functiongraph', [funcF], {
-                strokewidth: 2
-            }).setName("funcF");
+            board1.create('functiongraph', [funcF], { strokecolor: "#0000ff", strokewidth: 4 }).setName("funcF");
         } else if (id == "functionList2") {
-            funcG = func;
+            funcG = eval(value);
             board2.removeObject(board2.elementsByName.funcG);
-            board2.create('functiongraph', [funcG], ).setName("funcG");
+            board2.create('functiongraph', [funcG], { strokecolor: "#ff0000", strokewidth: 4 }).setName("funcG");
         }
     }
-    /*___________________________________________Board 1___________________________________________*/
-var board1 = JXG.JSXGraph.initBoard('FuncPlot', {
-    boundingbox: [-10, 10, 10, -10],
-    axis: true,
-    showCopyright: false
-});
-var A = board1.create('point', [1, 1], {
-    name: 'A'
-});
-board1.create('functiongraph', [funcF], { strokecolor: "#0000ff", strokewidth: 2, }).setName("funcF");
-board1.on("update", updateConv);
-/*________________________________________Board 2___________________________________________*/
-var board2 = JXG.JSXGraph.initBoard('FuncPlot2', {
-    boundingbox: [-10, 10, 10, -10],
-    axis: true,
-    showCopyright: false
-});
-var A2 = board2.create('point', [1, 1], {
-    name: 'A2'
-});
-board2.create('functiongraph', [funcG], { strokecolor: "#ff0000", strokewidth: 2, }).setName("funcG");
-board2.on("update", updateConv);
-/*___________________________________________Board 3___________________________________________*/
-var board3 = JXG.JSXGraph.initBoard("FuncPlot3", {
+    /*___________________________________Board 1 _________________________________________*/
+var board1 = JXG.JSXGraph.initBoard('FuncPlot', { //Board 1 Initialization
+    boundingbox: [-3, 3, 3, -3],
     axis: true,
     showCopyright: false,
     showNavigation: false,
-    boundingbox: [-10, 10, 10, -10],
-    /* zoom: {
-        factorX: 1.25, // horizontal zoom factor (multiplied to JXG.Board#zoomX)
-        factorY: 1.25, // vertical zoom factor (multiplied to JXG.Board#zoomY)
-        wheel: true, // allow zooming by mouse wheel or
-        // by pinch-to-toom gesture on touch devices
-        needShift: true, // mouse wheel zooming needs pressing of the shift key
-        min: 0.001, // minimal values of JXG.Board#zoomX and JXG.Board#zoomY, limits zoomOut
-        max: 1000.0, // maximal values of JXG.Board#zoomX and JXG.Board#zoomY, limits zoomIn
-
-        pinchHorizontal: true, // Allow pinch-to-zoom to zoom only horizontal axis
-        pinchVertical: true, // Allow pinch-to-zoom to zoom only vertical axis
-        pinchSensitivity: 7 // Sensitivity (in degrees) for recognizing horizontal or vertical pinch-to-zoom gestures.
-    }, */
-
-    /* keepAspectRatio: true,
-    pan: { enabled: true },
+});
+var A = board1.create('point', [1, 1], { name: '(X,Y)', }); // creation of Point A (Variable to Change the Value of the Functions)
+board1.create('functiongraph', [funcF], { strokecolor: "#0000ff", strokewidth: 4, }).setName("funcF"); //creation of functiongraph (FuncF) in board 1
+board1.on("update", updateConv); //Runs through most elements in Board 1 and calls their update() method and update board 3.
+/*________________________________________Board 2___________________________________________*/
+var board2 = JXG.JSXGraph.initBoard('FuncPlot2', {
+    boundingbox: [-3, 3, 3, -3],
     axis: true,
-    needsFullUpdate: true, */
-    // JXG.Coords(COORDS_BY_SCREEN, [-10, 10, 10, -10], board3, true),
-    //fullUpdate(),
-    //JXG.Board.BOARD_MODE_MOVE_ORIGIN,
-    //applyZoom() {},
-    //hasPoint(JXG.Coords): true,
+    showCopyright: false,
+    showNavigation: false,
 });
-sliderG = board3.create("slider", [
-    [-9, -1],
-    [9, -1],
-    [-10, -10, 10]
-], {
-    fillColor: "#ff0000",
-    strokeColor: "#ff0000",
-    size: 6,
-    withLabel: false,
-    withTicks: false,
+var A2 = board2.create('point', [1, 1], { name: '(X,Y)', color: "#0000ff" });
+board2.create('functiongraph', [funcG], { strokecolor: "#ff0000", strokewidth: 4, }).setName("funcG");
+board2.on("update", updateConv);
+/*___________________________________________Board 3___________________________________________*/
+var board3 = JXG.JSXGraph.initBoard("FuncPlot3", {
+    boundingbox: [-10, 10, 10, -10],
+    axis: true,
+    showCopyright: false,
+    showNavigation: false,
+
+    zoom: {
+        wheel: true, // allow zooming by mouse wheel or by pinch-to-toom gesture on touch devices
+        needShift: false, // mouse wheel zooming dont need pressing of the shift key
+    },
+    pan: { enabled: true, needShift: false }, //Control the possibilities for panning interaction 
+
 });
-////////////////////////////////////
+//Get slidervalue 
+sliderValue = function() {
+    value = document.getElementById("slider").value;
+    return value;
+};
+
 var funcFConv = board3.create("functiongraph", [function(x) {
     return funcF(x);
 }], {
-    strokewidth: 2,
+    strokewidth: 4,
 });
 
 var funcGConv = board3.create("functiongraph", [function(x) {
-    return funcG(sliderG.Value() - x);
+    return funcG(sliderValue() - x);
 }], {
-    strokewidth: 2,
+    strokewidth: 4,
     strokecolor: "#ff0000"
 });
 
+// http://hipersayanx.blogspot.de/2015/06/image-convolution.html
 var conv = board3.create("functiongraph", [function(t) {
-    if (t > sliderG.Value())
+    if (t > sliderValue())
         return Math.Nan;
     sum = 0
-    step = 0.01
-        // http://hipersayanx.blogspot.de/2015/06/image-convolution.html
+    step = 0.02
+    for (var x = -20; x < 20; x += step)
 
-
-
-    for (var x = -10; x < 10; x += step)
         sum += funcF(x) * funcG(t - x)
 
     return sum * step;
-}, -10, 10], {
-    strokewidth: 2,
+
+}, -20, 20], {
+    strokewidth: 4,
     strokecolor: "#000000",
-    /*  trace: false,
-     highlight: true,
-     scalable: true,
-     dragToTopOfLayer: false,
-     fillColor: '#565656',
-     strokeOpacity: 0.8,
-     fillOpacity: 0.8,
-     includeBoundaries: true, */
-
-
 
 });
-
+// Update function
 function updateConv() {
-    board3.update();
-    if (conv) board3.zoomElements([conv]);
+    // board3.update();
+    board3.zoomElements([conv]) // update the zoom level to fit convolution graph
+
+
 };
