d563225f2bf1ece459aaad113a2dc74f04f37d1f
MoRaMo11 <80282160+MoRaMo11@users.noreply.github.com>
2021-03-19 13:38:21 +0100

    Update FaltungDemo.js

--- FaltungDemo.js
+++ FaltungDemo.js
@@ -66,17 +66,18 @@ Saegezahn2 = function funcF(t) {
 funcG = Rechteck2; // default value for funcG
 /*____________________________________________________________________________ChangeFunc_______________________________________________________________________*/
 changeFunction = function(value, id) {
-        if (id == "functionList1") {
-            funcF = eval(value);
-            board1.removeObject(board1.elementsByName.funcF);
-            board1.create('functiongraph', [funcF], { strokecolor: "#0000ff", strokewidth: 4 }).setName("funcF");
-        } else if (id == "functionList2") {
-            funcG = eval(value);
-            board2.removeObject(board2.elementsByName.funcG);
-            board2.create('functiongraph', [funcG], { strokecolor: "#ff0000", strokewidth: 4 }).setName("funcG");
-        }
+    if (id == "functionList1") {
+        funcF = eval(value);
+        board1.removeObject(board1.elementsByName.funcF);
+        board1.create('functiongraph', [funcF], { strokecolor: "#0000ff", strokewidth: 4 }).setName("funcF");
+    } else if (id == "functionList2") {
+        funcG = eval(value);
+        board2.removeObject(board2.elementsByName.funcG);
+        board2.create('functiongraph', [funcG], { strokecolor: "#ff0000", strokewidth: 4 }).setName("funcG");
     }
-    /*___________________________________Board 1 _________________________________________*/
+}
+
+/*___________________________________Board 1 _________________________________________*/
 var board1 = JXG.JSXGraph.initBoard('FuncPlot', { //Board 1 Initialization
     boundingbox: [-3, 3, 3, -3],
     axis: true,
@@ -86,6 +87,7 @@ var board1 = JXG.JSXGraph.initBoard('FuncPlot', { //Board 1 Initialization
 var A = board1.create('point', [1, 1], { name: '(X,Y)', }); // creation of Point A (Variable to Change the Value of the Functions)
 board1.create('functiongraph', [funcF], { strokecolor: "#0000ff", strokewidth: 4, }).setName("funcF"); //creation of functiongraph (FuncF) in board 1
 board1.on("update", updateConv); //Runs through most elements in Board 1 and calls their update() method and update board 3.
+
 /*________________________________________Board 2___________________________________________*/
 var board2 = JXG.JSXGraph.initBoard('FuncPlot2', {
     boundingbox: [-3, 3, 3, -3],
@@ -96,9 +98,11 @@ var board2 = JXG.JSXGraph.initBoard('FuncPlot2', {
 var A2 = board2.create('point', [1, 1], { name: '(X,Y)', color: "#0000ff" });
 board2.create('functiongraph', [funcG], { strokecolor: "#ff0000", strokewidth: 4, }).setName("funcG");
 board2.on("update", updateConv);
+
 /*___________________________________________Board 3___________________________________________*/
 var board3 = JXG.JSXGraph.initBoard("FuncPlot3", {
     boundingbox: [-10, 10, 10, -10],
+    minBoundingBox: [-10, 10, 10, -10],
     axis: true,
     showCopyright: false,
     showNavigation: false,
@@ -106,13 +110,12 @@ var board3 = JXG.JSXGraph.initBoard("FuncPlot3", {
     zoom: {
         wheel: true, // allow zooming by mouse wheel or by pinch-to-toom gesture on touch devices
         needShift: false, // mouse wheel zooming dont need pressing of the shift key
-        pinchHorizontal: true, // Allow pinch-to-zoom to zoom only horizontal axis
-        pinchVertical: false, // Allow pinch-to-zoom to zoom only vertical axis
-        pinchSensitivity: 7, // Sensitivity (in degrees) for recognizing horizontal or vertical pinch-to-zoom gestures.
+        needTwoFingers: true
     },
-    pan: { enabled: true, needTwoFingers: true, needShift: false }, //Control the possibilities for panning interaction 
+    pan: { enabled: true, needTwoFingers: false, needShift: false }, //Control the possibilities for panning interaction 
 
 });
+
 //Get slidervalue 
 sliderValue = function() {
     value = document.getElementById("slider").value;
@@ -131,13 +134,12 @@ var funcGConv = board3.create("functiongraph", [function(x) {
     strokewidth: 4,
     strokecolor: "#ff0000"
 });
-
 // http://hipersayanx.blogspot.de/2015/06/image-convolution.html
 var conv = board3.create("functiongraph", [function(t) {
     if (t > sliderValue())
         return Math.Nan;
     sum = 0
-    step = 0.02
+    step = 0.01
     for (var x = -20; x < 20; x += step)
 
         sum += funcF(x) * funcG(t - x)
@@ -149,10 +151,8 @@ var conv = board3.create("functiongraph", [function(t) {
     strokecolor: "#000000",
 
 });
+
 // Update function
 function updateConv() {
-    // board3.update();
-    board3.zoomElements([conv]) // update the zoom level to fit convolution graph
-
-
+    board3.zoomElements([conv]); // update the zoom level to fit convolution graph*/
 };
